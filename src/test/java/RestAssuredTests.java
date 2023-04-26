e5791a3014c11aba887c07ff4fa018071a04d630
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2023-04-27 01:02:48 +0300

    Научились парсить json: по ключу получать значение

--- src/test/java/RestAssuredTests.java
+++ src/test/java/RestAssuredTests.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -7,7 +8,7 @@ import java.util.Map;
 
 public class RestAssuredTests {
   @Test
-  public void testGetHello(){
+  public void testRestAssured(){
     Map<String, String> params = new HashMap<>();
     params.put("name", "John");
 
@@ -18,5 +19,26 @@ public class RestAssuredTests {
             .get(" https://playground.learnqa.ru/api/hello")
             .andReturn();
     response.prettyPrint();
+  }
+
+  @Test
+  public void testJsonParsing(){
+    Map<String, String> params = new HashMap<>();
+    params.put("name", "John");
+
+
+    JsonPath response = RestAssured
+            .given()
+            .queryParams(params)
+            .get(" https://playground.learnqa.ru/api/hello")
+            .jsonPath();
+
+    String answer = response.get("answer1");
+    if (answer != null) {
+      System.out.println(answer);
+    } else {
+      System.out.println("The key is absent");
+    }
+
   }
 }
