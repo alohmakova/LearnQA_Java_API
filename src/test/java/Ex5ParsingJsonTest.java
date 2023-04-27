c8ce442d94c8d8e2f47f9852474280720e220b3b
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2023-04-28 00:08:55 +0300

    Ex5: Парсинг JSON

---
+++ src/test/java/Ex5ParsingJsonTest.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
+
+public class Ex5ParsingJsonTest {
+  @Test
+  public void testEx5ParsingJson(){
+
+    JsonPath response = RestAssured
+            .given()
+            //.queryParams(params)
+            .get("https://playground.learnqa.ru/api/get_json_homework")
+            .jsonPath();
+
+   ArrayList<Object> messages = response.get("messages");
+    if (messages != null) {
+      System.out.println("\nПолученный JSON " + messages);
+    } else {
+      System.out.println("There is no messages");
+    }
+
+    String secondMessage = response.getString("messages[1].message");
+    System.out.println("\nТекст второго сообщения: " + secondMessage);
+
+  }
+
+
+}
