eaaaf2b6d3b12666c027d4abfcefdf6f969933a5
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2023-05-05 20:28:05 +0300

    Создан базовый класс BaseTestCase

--- src/test/java/tests/UserAuthTest.java
+++ src/test/java/tests/UserAuthTest.java
@@ -1,8 +1,11 @@
+package tests;
+
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestLogSpecification;
 import io.restassured.specification.RequestSpecification;
+import lib.BaseTestCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import io.restassured.http.Headers;
@@ -15,7 +18,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class UserAuthTest {
+public class UserAuthTest extends BaseTestCase {
   String cookie;
   String header;
   int userIdOnAuth;
@@ -32,9 +35,9 @@ public void loginUser(){
           .post("https://playground.learnqa.ru/api/user/login")
           .andReturn();
 
-  this.cookie = responseGetAuth.getCookie("auth_sid");
-  this.header = responseGetAuth.getHeader("x-csrf-token");
-  this.userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+  this.cookie = this.getCookie(responseGetAuth, "auth_sid");
+  this.header = this.getHeader(responseGetAuth, "x-csrf-token");
+  this.userIdOnAuth = this.getIntFromJson(responseGetAuth, "user_id");
   }
 
   @Test
