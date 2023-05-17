d1654a69f1212dc0cfabdc42faa2256c328bc2f4
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2023-05-17 18:30:35 +0300

    Ex16: Запрос данных другого пользователя

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -1,16 +1,29 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Epic("Authorisation cases")
+@Feature("Get user information")
 public class UserGetTest extends BaseTestCase {
+  private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+  String cookie;
+  String header;
+
   @Test
+  @Description("This test checks user information w/o authorization")
+  @DisplayName("Test negative user information")
   public void testGetUserDataNotAuth(){
     Response responseUserData = RestAssured
            .get("https://playground.learnqa.ru/api/user/2")
@@ -24,6 +37,8 @@ public class UserGetTest extends BaseTestCase {
   }
 
   @Test
+  @Description("This test checks user information after authorization")
+  @DisplayName("Test positive user information")
   public void testGetUserDetailsAuthAsSameUser(){
     Map<String, String> authData = new HashMap<>();
     authData.put("email", "vinkotov@example.com");
@@ -50,6 +65,37 @@ public class UserGetTest extends BaseTestCase {
 
     String[] expectedFieldNames = {"username", "firstName", "lastName", "email"};
     Assertions.assertJsonHasFields(responseUserData, expectedFieldNames);
+  }
+  @Test
+  @Description("This test checks user information from another account")
+  @DisplayName("Test negative user information")
+  public void testGetUserDetailsAuthAsAnotherUser(){
+    Map<String, String> authData = new HashMap<>();
+    authData.put("email", "vinkotov@example.com");
+    authData.put("password", "1234");
+
+
+    Response responseGetAuth = apiCoreRequests
+            .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+    this.cookie = this.getCookie(responseGetAuth, "auth_sid");
+    this.header = this.getHeader(responseGetAuth, "x-csrf-token");
+
+
+    Response responseUserData = apiCoreRequests
+            .makeGETRequest(
+                    "https://playground.learnqa.ru/api/user/187",
+                    this.header,
+                    this.cookie
+            );
+
+
+    System.out.println(responseUserData.asString());
+
+    Assertions.assertJsonHasField(responseUserData, "username");
+    Assertions.assertJsonHasNotField(responseUserData, "firstName");
+    Assertions.assertJsonHasNotField(responseUserData, "lastName");
+    Assertions.assertJsonHasNotField(responseUserData, "email");
   }
 
 }
